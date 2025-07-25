e6ac663c55a6bb54405c53a025ef30cf3c3db73a
syma sultana <symasultana2002@gmail.com>
2025-07-25 21:33:24 +0600

    first commit

---
+++ src/assets/Routes.jsx
@@ -0,0 +1,14 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
+export default router;
